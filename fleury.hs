4b3f86db902fe942b62e79343efa6168f5772d09
tarcito <tarcito.lua@gmail.com>
2018-07-12 10:01:06 -0300

    implementado eh_Conexo

--- fleury.hs
+++ fleury.hs
@@ -7,16 +7,14 @@ eh_Digrafo :: (Eq a) => (Set a, Set (a, a)) -> Bool
 eh_Digrafo ((Set xs) , (Set [])) = True
 eh_Digrafo ((Set xs) , (Set (y:ys))) = ( exists (snd y, fst y) (Set ys)) && eh_Digrafo ((Set xs) , Set (filter ( /= (snd y, fst y)) ys))
 
-
+eh_Conexo ::  (Eq a) => (Set a, Set (a, a)) -> Bool
+eh_Conexo ((Set []), (Set ys)) = True
+eh_Conexo ((Set (x:xs)), (Set ys)) = ((length (filter (\z -> (fst z == x)) ys)) > 0 ) &&  ((length (filter (\z -> (snd z == x)) ys)) > 0 ) && 
+                                      eh_Conexo ((Set xs), (Set ys) )
 
 -- To Do ...
-
---eh_Conexo ::  (Eq a) => (Set a, Set (a, a)) -> Bool
-eh_Conexo ((Set []), (Set [])) = True
-eh_Conexo ((Set (x:xs)), (Set (y:ys)) = filter (\z -> (fst z == x))  
 
 -- eh_Euleriano ::  (Eq a) => (Set a, Set (a, a)) -> Bool
 eh_Euleriano ((Set (x:xs)),(Set ys)) =  ((length (filter (\z -> fst z == x))) >= 2) &&  
 
 
--}
