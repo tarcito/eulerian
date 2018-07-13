7f7c09210bf5f5b7f0f0bd022a241f3a0435108f
tarcito <tarcito.lua@gmail.com>
2018-07-13 19:45:15 -0300

    inicio da impl. de DFS

--- fleury.hs
+++ fleury.hs
@@ -7,19 +7,38 @@ eh_Digrafo :: (Eq a) => (Set a, Set (a, a)) -> Bool
 eh_Digrafo ((Set xs) , (Set [])) = True
 eh_Digrafo ((Set xs) , (Set (y:ys))) = ( exists (snd y, fst y) (Set ys)) && eh_Digrafo ((Set xs) , Set (filter ( /= (snd y, fst y)) ys))
 
+
+-- To Do ...
+depth_First_Search ((Set []), (Set ys)) = [] 
+depth_First_Search ((Set (x:xs)), (Set ys)) = d_F_S x ys
+
+d_F_S v ys = v: d_F_S filter  ys
+
+
+
+-- To Do ...
 eh_Conexo ::  (Eq a) => (Set a, Set (a, a)) -> Bool
+eh_Conexo ((Set xs), (Set ys)) = [(x, y)| x <- xs, y <- ys] ++ 
+
+{-
 eh_Conexo ((Set []), (Set ys)) = True
 eh_Conexo ((Set (x:xs)), (Set ys)) = (((length (filter (\z -> (fst z == x)) ys)) > 0 ) ||  ((length (filter (\z -> (snd z == x)) ys)) > 0 )) && 
                                       eh_Conexo ((Set xs), (Set ys) )
+
+-}
 
 eh_Euleriano ::  (Eq a) => (Set a, Set (a, a)) -> Bool
 eh_Euleriano ((Set []), (Set ys)) = True
 eh_Euleriano ((Set (x:xs)),(Set ys)) =  (mod (length (filter (\z -> fst z == x) ys)) 2 == 0) &&  (mod (length (filter (\z -> snd z == x) ys)) 2 == 0) && 
               eh_Euleriano ((Set xs), (Set ys))
 
+--eh_Ponte :: (Eq a) => (a, a) -> (Set a, Set (a, a)) -> Bool 
+eh_Ponte (a, b) ((Set xs), (Set ys)) = not (eh_Conexo ((Set xs), (Set (filter (\z -> (z /= (b, a)) && (z /= (a, b))) ys))) )
+
+
+
 
 -- To Do ...
-
 --eulerian_Path :: (Eq a) => (Set a, Set (a, a)) -> [a]
 eulerian_Path grafo |((eh_Conexo grafo) && (eh_Digrafo grafo) && (eh_Euleriano)) = eulerian_Path' grafo
                     |otherwise = error "Grafo não euleriano"
@@ -29,6 +48,6 @@ eulerian_Path = ((Set xs), (Set [])) = []
 eulerian_Path = ((Set (x:xs)), (Set (y:ys))) = 
 
 
-
 -- Exemplo Grafo euleriano simples
 g = addEdges [(1,2),(2,1),(1,5),(5,1),(2,3),(3,2),(3,4),(4,3),(4,5),(5,4)] (addVertices [1,2,3,4,5] graph)
+
