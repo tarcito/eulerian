73846416a42ccbb7c25d793109ed61582145e20e
tarcito <tarcito.lua@gmail.com>
2018-07-13 22:45:01 -0300

    inicio da func. eulerian_Path

--- fleury.hs
+++ fleury.hs
@@ -4,12 +4,12 @@ import Graph
 import Set
 
 
-eh_Digrafo :: (Eq a) => (Set a, Set (a, a)) -> Bool
+eh_Digrafo :: Eq a => (Set a, Set (a, a)) -> Bool
 eh_Digrafo ((Set xs) , (Set [])) = True
 eh_Digrafo ((Set xs) , (Set (y:ys))) = ( exists (snd y, fst y) (Set ys)) && eh_Digrafo ((Set xs) , Set (filter ( /= (snd y, fst y)) ys))
 
 
-eh_Conexo ::  (Eq a) => (Set a, Set (a, a)) -> Bool
+eh_Conexo :: Eq a => (Set a, Set (a, a)) -> Bool
 eh_Conexo ((Set []), (Set ys)) = True
 eh_Conexo ((Set (x:xs)), (Set ys)) = (((length (filter (\z -> (fst z == x)) ys)) > 0 ) ||  ((length (filter (\z -> (snd z == x)) ys)) > 0 )) && 
                                       eh_Conexo ((Set xs), (Set ys) )
@@ -19,7 +19,7 @@ eh_Ponte :: Eq a => (a, a) -> (Set a, Set (a, a)) -> Bool
 eh_Ponte (a, b) ((Set xs), (Set ys)) = not (eh_Conexo ((Set xs), (Set (filter (\z -> (z /= (b, a)) || (z /= (a, b))) ys))) )
 
 
---eh_Euleriano ::  (Eq a) => (Set a, Set (a, a)) -> Bool
+eh_Euleriano ::  Eq a => (Set a, Set (a, a)) -> Bool
 eh_Euleriano ((Set []), (Set ys)) = True
 eh_Euleriano ((Set (x:xs)),(Set ys)) =  (mod (length (filter (\z -> fst z == x) ys)) 2 == 0) &&  (mod (length (filter (\z -> snd z == x) ys)) 2 == 0) && 
               eh_Euleriano ((Set xs), (Set ys))
@@ -34,14 +34,15 @@ d_F_S v (y:ys) = v: d_F_S filter  ys
 
 
 -- To Do ...
-eulerian_Path :: (Eq a) => (Set a, Set (a, a)) -> [a]
+eulerian_Path :: Eq a => (Set a, Set (a, a)) -> [a]
 eulerian_Path grafo |((eh_Conexo grafo) && (eh_Digrafo grafo) && (eh_Euleriano grafo)) = eulerian_Path' grafo
                     |otherwise = error "Grafo não euleriano"
 
 eulerian_Path' ((Set []), (Set [])) = []
 eulerian_Path' ((Set xs), (Set [])) = []
---eulerian_Path' ((Set (x:xs)), (Set (y:ys))) = Nothing
-
+eulerian_Path' ((Set (x:xs)), (Set ys)) |((length (arestas x ys)) > 1) = Nothing
+   where arestas  v as = filter (\y -> fst y == v) as
+         all_Aresta v as = (arestas v as) ++ (filter (\y -> snd y == v) as) 
 
 
 -- Exemplo Grafo euleriano simples
