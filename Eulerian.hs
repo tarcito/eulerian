473be5e1f2c3f172f13def299cb17b0941c6a712
tarcito <tarcito.lua@gmail.com>
2018-07-31 21:13:25 -0300

    implementado eh_Semi_Euleriano e correções de assinatura

--- Eulerian.hs
+++ Eulerian.hs
@@ -1,5 +1,5 @@
 {-
-  implementação do Algoritmo de checa a existencia de grafo euleriano, com paradgma funcional (Haskell)
+  implementação de algoritmo que checa a existencia de grafo euleriano ou semi-euleriano, com paradgma funcional (Haskell)
   authors: Alekssandro e Tárcito
 -}
 
@@ -20,28 +20,30 @@ eh_Conexo ((Set (x:xs)), (Set ys)) = (((length (filter (\z -> (fst z == x)) ys))
 eh_Ponte :: Eq a => (a, a) -> (Set a, Set (a, a)) -> Bool 
 eh_Ponte (a, b) ((Set xs), (Set ys)) = not (eh_Conexo ((Set xs), (Set (filter (\z -> (z /= (b, a)) || (z /= (a, b))) ys))) )
 
--- To Do ...
 eh_Semi_Euleriano :: Eq a => (Set a, Set (a, a)) -> Bool
-eh_Semi_Euleriano ((Set xs), (Set ys)) = eh_Semi_Euleriano' 0 ((Set xs),(Set ys))
+eh_Semi_Euleriano ((Set xs), (Set ys)) = eh_Semi_Euleriano' 0 [] ((Set xs),(Set ys))
 
-eh_Semi_Euleriano' z ((Set []), (Set ys)) = z == 2
-eh_Semi_Euleriano' z ((Set (x:xs)),(Set ys)) =  if( ((length (filter (\z -> fst z == x) ys)) == 1) ||  ((length (filter (\z -> snd z == x) ys)) == 1) ) then eh_Semi_Euleriano' (z+1) ((Set xs), (Set ys))
-              else eh_Semi_Euleriano' z ((Set xs), (Set ys))
+eh_Semi_Euleriano' z zs ((Set []), (Set ys)) = (z == 2) && eh_Euleriano ((Set zs), (Set ys))
+eh_Semi_Euleriano' z zs ((Set (x:xs)),(Set ys)) =  if( ((length (filter (\z -> fst z == x) ys)) == 1) ||  ((length (filter (\z -> snd z == x) ys)) == 1) ) then eh_Semi_Euleriano' (z+1) zs ((Set xs), (Set ys))
+              else eh_Semi_Euleriano' z (x:zs) ((Set xs), (Set ys))
 
 eh_Euleriano ::  Eq a => (Set a, Set (a, a)) -> Bool
 eh_Euleriano ((Set []), (Set ys)) = True
 eh_Euleriano ((Set (x:xs)),(Set ys)) =  (mod (length (filter (\z -> fst z == x) ys)) 2 == 0) &&  (mod (length (filter (\z -> snd z == x) ys)) 2 == 0) && 
               eh_Euleriano ((Set xs), (Set ys))
 
+grau_Vertices :: Eq a => (Set a, Set (a, a)) -> [(a, Int)]
 grau_Vertices ((Set []), (Set ys)) = []
 grau_Vertices ((Set (x:xs)),(Set ys)) = (x, (length (filter (\z -> fst z == x) ys)) ) : grau_Vertices ((Set xs),(Set ys)) 
 
-{-- To Do ...
+{--
+-- To Do ...
 depth_First_Search ((Set []), (Set ys)) = [] 
 depth_First_Search ((Set (x:xs)), (Set ys)) = depth_First_Search((Set d_F_S x (filter  ys), (Set ys))
 
 d_F_S v (y:ys) = v: d_F_S filter  ys
 
+-- To Do ...
 eulerian_Path :: Eq a => (Set a, Set (a, a)) -> [a]
 eulerian_Path grafo |((eh_Conexo grafo) && (eh_Digrafo grafo) && (eh_Euleriano grafo)) = eulerian_Path' grafo
                     |otherwise = error "Grafo não euleriano"
